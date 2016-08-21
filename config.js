9de42387d250b544399d956bd018e007d411ee78
Mark Fajet <mfajet@users.noreply.github.com>
2016-08-21 18:55:13 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  googleKey: '',//obtained from https://developers.google.com/maps/documentation/geocoding/get-api-key
+  //Make sure to have a chipotke account with a previous order
+  chipotleEmail: '',
+  chiptolePassword: '',
+  chipotlePhoneNum: '',//format: 555.555.5555
+  //Your twitter user ID http://mytwitterid.com/
+  twitterUserID: '',
+  //https://apps.twitter.com/
+  twitterConsumerKey: "",
+  twitterConsumerSecret: "",
+  twitterAccessToken: "",
+  twitterAccessTokenSecret: "",
+}
